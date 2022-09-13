fec0b8ebe9470a835db598c3ae12e9455a5034aa
Sam Phipps <samuel.v.phipps@gmail.com>
2022-09-13 10:12:06 -0500

    Finished adding 6 songs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,17 @@
 console.log('***** Music Collection *****')
 let collection=[];
-function addToCollection(title, artist, yearPublished){
- let object={};
+function addToCollection(title, artist, yearPublished){   
+let object={};
  object.title=title;
  object.artist=artist;
  object.yearPublished=yearPublished;
  collection.push(object);
+ return object;
 }
+
 addToCollection('2Pac', 'California Love', 1995);
 addToCollection('Chopin', 'Ballade No. 1', 1836);
-addToCollection
+addToCollection('Chopin', 'Andante spianato et grande polonaise brillante', 1834);
+addToCollection('Neil Young', 'Harvest Moon', 1992);
+addToCollection('Daughter', 'Loudon Wainwright III', 2007);
+addToCollection('Brooklyn Ferry', 'Holy Hive', 2021);
