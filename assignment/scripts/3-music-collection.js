9a6633fb8040683f6de869a0365986d79c84d1c6
Sam Phipps <samuel.v.phipps@gmail.com>
2022-09-13 10:47:15 -0500

    Stretch goal requirements confusing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,13 +14,13 @@ addToCollection('California Love', '2Pac', 1995);
 addToCollection('Ballade No. 1', 'Chopin', 1836);
 addToCollection('Andante spianato et grande polonaise brillante', 'Chopin', 1834);
 addToCollection('Harvest Moon', 'Neil Young', 1992);
-addToCollection('Daughter', 'Loudon Wainwright III', 2007);
-addToCollection('Brooklyn Ferry', 'Holy Hive', 2021);
+addToCollection('Strange Wierdos', 'Loudon Wainwright III', 2007);
+addToCollection('Holy Hive', 'Holy Hive', 2021);
 
-console.log('list of songs:', collection);
+console.log('list of albums:', collection);
 
 function showCollection(array){
-    console.log('Number of songs in collection:', array.length);
+    console.log('Number of albums in collection:', array.length);
     for(i=0; i<array.length; i++){
         console.log(array[i].title, 'by', array[i].artist, 'published in', array[i].yearPublished);
     };
@@ -42,3 +42,7 @@ return songList;
 console.log('Searching for Chopin in array, should return two objects:', findByArtist('Chopin', collection));
 
 console.log('Searching for Mylie Cyrus in array, should return empty array:', findByArtist('Mylie Cyrus', collection));
+
+
+//STRETCH GOALS
+//I dont fully understand what the stretch goals are asking me to do. Instructions are confusing.
