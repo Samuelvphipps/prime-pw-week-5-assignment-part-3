fb0354101fe3ccf3985ddaee16053c053c5a9a8d
Sam Phipps <samuel.v.phipps@gmail.com>
2022-09-13 10:04:41 -0500

    Finished function and started adding songs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+let collection=[];
+function addToCollection(title, artist, yearPublished){
+ let object={};
+ object.title=title;
+ object.artist=artist;
+ object.yearPublished=yearPublished;
+ collection.push(object);
+}
+addToCollection('2Pac', 'California Love', 1995);
+addToCollection('Chopin', 'Ballade No. 1', 1836);
+addToCollection
